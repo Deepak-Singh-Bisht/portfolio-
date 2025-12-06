fec1311fb6bd64e2001286035c4f6c1772ffa43c
DEEPAK SINGH BISHT <110534750+Deepak-Singh-Bisht@users.noreply.github.com>
2025-12-06 22:11:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Close menu when a link is clicked
+document.querySelectorAll('.nav-links li a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
